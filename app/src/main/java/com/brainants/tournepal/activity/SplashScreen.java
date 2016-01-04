d5a4e03c8c5db2a46c0801d42e4798ae7ea9f46d
awazgyawali <awazgyawali@gmail.com>
2016-01-04 07:29:11 +0545

    Before SplashScreen

---
+++ app/src/main/java/com/brainants/tournepal/activity/SplashScreen.java
@@ -0,0 +1,15 @@
+package com.brainants.tournepal.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.brainants.tournepal.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
